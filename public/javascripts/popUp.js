ae830a0869ea35b18b38f5e9b6c9c0897f7c99f2
michael_b <m.brueggemann7@web.de>
2023-07-24 12:05:07 +0200

    added basic idea for PopUp

---
+++ public/javascripts/popUp.js
@@ -0,0 +1,8 @@
+const closeButton = document.getElementById("close-button")
+const modal = document.getElementById("modal")
+
+closeButton.addEventListener("click", () => 
+{
+    modal.close()
+})
+
