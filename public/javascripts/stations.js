deeb3f8bb445f3469be7782a591f6ce1ad26c357
michael_b <m.brueggemann7@web.de>
2023-08-10 18:10:18 +0200

    umstrukturierung station forms

--- public/javascripts/stations.js
+++ public/javascripts/stations.js
@@ -163,11 +163,7 @@ function initializeMap()
     let drawnItems = new L.FeatureGroup()
     map.addLayer(drawnItems)
 
-    // LayerGroup to later store stations
-    let stations_layer_group = L.layerGroup()
-    map.addLayer(stations_layer_group)
-
-    // initialize DrawControl Toolbar
+    // initialise draw control
     let drawControl = new L.Control.Draw({
         draw: {
             // only allow polygon and points to be drawn
@@ -182,7 +178,10 @@ function initializeMap()
             featureGroup: drawnItems
         }
     })
-    map.addControl(drawControl)
+
+    // LayerGroup to later store stations
+    let stations_layer_group = L.layerGroup()
+    map.addLayer(stations_layer_group)
 
     // save drawn items 
     map.on(L.Draw.Event.CREATED, function (event) 
@@ -197,7 +196,7 @@ function initializeMap()
         drawnItems.addLayer(layer)
 
         // adds the geojson of the layer to the textarea, so that users can edit the metadata
-        let textarea = document.getElementById("textarea_geoJSON")
+        let textarea = document.getElementById("hidden_geojson_data_from_map_feature")
         textarea.value = JSON.stringify(layer.toGeoJSON(), null, 2)
     })
 
@@ -224,7 +223,8 @@ function initializeMap()
     return {
         map: map,
         stations_layer_group: stations_layer_group,
-        drawnItems: drawnItems
+        drawnItems: drawnItems,
+        drawControl: drawControl
     }
 }
 
@@ -262,12 +262,221 @@ async function update_map()
     }
 }
 
+// ----------------- Station Form -----------------
+
+function enter_add_station_mode(edit_style)
+{
+    switch(edit_style)
+    {
+        case "map":
+            // hide buttons
+            document.getElementById("add_station_button_area").style.display = "none"
+            // show map form
+            document.getElementById("map_form").style.display = "block"
+            break
+        case "textarea":
+            // hide buttons
+            document.getElementById("add_station_button_area").style.display = "none"
+            // show geojson_texarea form
+            document.getElementById("geojson_textarea_form").style.display = "block"
+            break
+        case "file-upload":
+            // hide buttons
+            document.getElementById("add_station_button_area").style.display = "none"
+            // show geojson_texarea form
+            document.getElementById("geojson_upload_form").style.display = "block"
+            break
+
+    }
+
+}
+
+/**
+ * Hides the currently shown form and returns to the button view "#add_station_button_area"
+ * @param {} form - Form that should be hidden
+ */
+function leave_add_station_mode(form)
+{
+    // hide form element
+    form.style.display = "none"
+    // show buttons
+    document.getElementById("add_station_button_area").style.display = "block"
+}
+
+/**
+ * This functions sole purpose is to increase readability of this code. 
+ * It wraps the initialisation of the buttons to choose in which style the user wants to add a new station and to leave the form.
+ * */ 
+function prepare_form_buttons()
+{
+    
+    // open_map_form button
+    document.getElementById("open_map_form").addEventListener("click", function()
+    {
+        // add stations via map
+        enter_add_station_mode("map")
+
+        // activate draw control on map
+        map.addControl(drawControl)
+    })
+
+    // submit_map_form button
+    document.getElementById("submit_map_form").addEventListener("click", function(event)
+    {
+        event.preventDefault()
+        let form = document.getElementById('map_form')
+        let formData = new FormData(form)
+        console.log(formData)
+        
+        // parse body from formData
+        let request_body = 
+        {
+            type: JSON.parse(formData.get("hidden_geojson_data_from_map_feature")).type,
+            properties: {
+                name: formData.get("input_name"),
+                description: formData.get("input_description"),
+                url: formData.get("input_url")
+            },
+            geometry: JSON.parse(formData.get("hidden_geojson_data_from_map_feature")).geometry
+        }
+        add_new_station(request_body)
+
+        form.reset()
+
+        // resets all elements drawn with the draw-tool
+        drawnItems.clearLayers() 
+        leave_add_station_mode(this.parentElement)
+    })
+
+
+    // cancel_map_form button
+    document.getElementById("cancel_map_form").addEventListener("click", function()
+    {
+        // hide coressponding form element
+        leave_add_station_mode(this.parentElement)
+
+        // deactivate draw control on map
+        map.removeControl(drawControl)
+    })
+
+    // open_geojson_textarea_form button
+    document.getElementById("open_geojson_textarea_form").addEventListener("click", function()
+    {
+        // add stations via textarea
+        enter_add_station_mode("textarea")
+    })
+
+    //TODO: Funktionalität zum "Daten absenden ergänzen"
+
+    // cancel_geojson_textarea_form button
+    document.getElementById("cancel_geojson_textarea_form").addEventListener("click", function()
+    {
+        // hide coressponding form element
+        let coressponding_form = this.parentElement
+        leave_add_station_mode(coressponding_form)
+        // reset form
+        coressponding_form.reset()
+    })
+
+    // show_sample_geojson button
+    document.getElementById("show_sample_geojson").addEventListener("click", function()
+    {
+        let textarea = document.getElementById("textarea_geoJSON")
+        textarea.value = JSON.stringify(
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Prinzipalmarkt",
+                "description": "Das ist der Prinzipalmarkt",
+                "url": "https://de.wikipedia.org/wiki/Prinzipalmarkt"
+            },
+            "geometry": {
+              "coordinates": [
+                7.628199238097352,
+                51.962239849033296
+              ],
+              "type": "Point"
+            }
+        }, null, 2)
+    })
+
+    // open_geojson_upload_form button
+    document.getElementById("open_geojson_upload_form").addEventListener("click", function()
+    {
+        // add stations via textarea
+        enter_add_station_mode("file-upload")
+    })
+        //TODO: Funktionalität zum "Daten absenden ergänzen"
+
+
+    // cancel_geojson_upload_form button
+    document.getElementById("cancel_geojson_upload_form").addEventListener("click", function()
+    {
+        // hide coressponding form element
+        leave_add_station_mode(this.parentElement)
+    })
+ 
+}
+
+/**
+ * This functions sole purpose is to increase readability of this code. 
+ * It wraps the initialisation of the geojson_file_upload input element
+ * */ 
+function prepare_geojson_file_ulpoad()
+{
+    // adds the file content uploaded to the input field to the textarea
+    document.getElementById('file_upload_geoJSON').addEventListener('change', function(event) 
+    {
+        let file = event.target.files[0]
+
+        // if no file is inputed
+        if (!file) return
+    
+        let reader = new FileReader();
+        reader.onload = function(event) 
+        {
+        let contents = event.target.result
+        document.getElementById('textarea_geoJSON').value = contents
+        }
+        reader.readAsText(file)
+    })
+}
+
+// ggf. löschen
+// //set in own scope to prevent unwanted global variables
+// {
+// const SUBMIT_STATION_FORM_BUTTON = document.getElementById("submit_station")
+// SUBMIT_STATION_FORM_BUTTON.addEventListener("click", function(event)
+// {
+//     //event.preventDefault()
+//     let form = document.getElementById('station_upload_form')
+//     let formData = new FormData(form)
+    
+    
+//     // parse body from formData
+//     let request_body = 
+//     {
+//         type: JSON.parse(formData.get("textarea_geoJSON")).type,
+//         properties: {
+//             name: formData.get("input_name"),
+//             description: formData.get("input_description"),
+//             url: formData.get("input_url")
+//         },
+//         geometry: JSON.parse(formData.get("textarea_geoJSON")).geometry
+//     }
+//     add_new_station(request_body)
+//     form.reset()
+//     drawnItems.clearLayers() // resets all elements drawn with the draw-tool
+// })
+// }
+
 // ----------------- Script Start -----------------
 
 let map_init = initializeMap()
 let map = map_init.map
 let stations_layer_group = map_init.stations_layer_group
 let drawnItems = map_init.drawnItems
+let drawControl = map_init.drawControl
 
 /* Use "event delegation" to bind an event to a detached button. The button "#update_station" is first attached to the DOM, if the modal "'edit_station_popup"
  is opened (the button is part of the modal), therefore pre-existing events for this button might get lost.*/
@@ -281,51 +490,8 @@ document.body.addEventListener('click', function(event) {
     }
 })
 
-// ----------------- Submit Field -----------------
-
-// adds the file content uploaded to the input field to the textarea
-document.getElementById('file_upload_geoJSON').addEventListener('change', function(event) 
-{
-    console.log(event.target)
-    let file = event.target.files[0]
-
-    // if no file is inputed
-    if (!file) return
-  
-    let reader = new FileReader();
-    reader.onload = function(event) 
-    {
-      let contents = event.target.result
-      document.getElementById('textarea_geoJSON').value = contents
-    }
-    reader.readAsText(file)
-  })
-
-//set in own scope to prevent unwanted global variables
-{
-const SUBMIT_BUTTON = document.getElementById("submit_station")
-SUBMIT_BUTTON.addEventListener("click", function(event)
-{
-    event.preventDefault()
-    let form = document.getElementById('station_upload_form')
-    let formData = new FormData(form)
-    
-    // parse body from formData
-    let request_body = 
-    {
-        type: JSON.parse(formData.get("textarea_geoJSON")).type,
-        properties: {
-            name: formData.get("input_name"),
-            description: formData.get("input_description"),
-            url: formData.get("input_url")
-        },
-        geometry: JSON.parse(formData.get("textarea_geoJSON")).geometry
-    }
-    add_new_station(request_body)
-    form.reset()
-    drawnItems.clearLayers() // resets all elements drawn with the draw-tool
-})
-}
+prepare_form_buttons()
+prepare_geojson_file_ulpoad()
 
 update_map()
 update_table()
