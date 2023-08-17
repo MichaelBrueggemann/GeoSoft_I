0ead3572d8edf7a64fe5d1cf4fdce537993816ec
tciroth <timciroth@uni-muenster.de>
2023-08-17 13:48:49 +0200

    einheitlich underscore-schreibweise (api)

--- routes/api.js
+++ routes/api.js
@@ -11,12 +11,12 @@ const FS = require('fs') // Read files from the file system
 // --------------- DATABASE INITIALIZATION ---------------
 
 const { ObjectId } = require('mongodb');
-const MongoClient = require('mongodb').MongoClient;
+const MONGO_CLIENT = require('mongodb').MongoClient;
 const URL = 'mongodb://127.0.0.1:/3000'; // connection URL
-const CLIENT = new MongoClient(URL); // mongodb client
-const DBNAME = 'mydatabase'; // database name
-const COLLECTIONNAMETOURS = 'touren'; // collection name
-const COLLECTIONNAMESTATIONS = 'stations'; // collection name
+const CLIENT = new MONGO_CLIENT(URL); // mongodb client
+const DB_NAME = 'mydatabase'; // database name
+const COLLECTION_NAME_TOURS = 'touren'; // collection name
+const COLLECTION_NAME_STATIONS = 'stations'; // collection name
 let db // Database-Instance where all data is stored
 let tour_collection; // Collection-Instance to perform database operations on
 let station_collection; // Collection-Instance to perform database operations on
@@ -24,16 +24,16 @@ let station_collection; // Collection-Instance to perform database operations on
 /**
  * Function to initialize the Database and Collection and store them in global variables for later access.
  */
-async function initializeDB()
+async function initialize_DB()
 {
   try
   {
     await CLIENT.connect();
-    db = CLIENT.db(DBNAME);
-    station_collection = db.collection(COLLECTIONNAMESTATIONS);
-    console.log(`Erfolgreich mit '${DBNAME}.${COLLECTIONNAMESTATIONS}' verbunden`);
-    tour_collection = db.collection(COLLECTIONNAMETOURS);
-    console.log(`Erfolgreich mit '${DBNAME}.${COLLECTIONNAMETOURS}' verbunden`);
+    db = CLIENT.db(DB_NAME);
+    station_collection = db.collection(COLLECTION_NAME_STATIONS);
+    console.log(`Erfolgreich mit '${DB_NAME}.${COLLECTION_NAME_STATIONS}' verbunden`);
+    tour_collection = db.collection(COLLECTION_NAME_TOURS);
+    console.log(`Erfolgreich mit '${DB_NAME}.${COLLECTION_NAME_TOURS}' verbunden`);
   }
   catch (err)
   {
@@ -41,7 +41,7 @@ async function initializeDB()
   }
 }
 
-initializeDB()
+initialize_DB()
 
 // ------------------- DB-Functions -------------------
 /**
@@ -52,9 +52,9 @@ initializeDB()
 async function add_item(item, collection)
 {
   try {
-    const result = await collection.insertOne(item);
+    const RESULT = await collection.insertOne(item);
     console.log('Neues Element in die Datenbank eingefügt');
-    console.log('Eingefügte ID:', result.insertedId);
+    console.log('Eingefügte ID:', RESULT.insertedId);
   } catch (err) {
     console.error('Fehler beim Einfügen des Elements in die Datenbank:', err);
   } 
@@ -67,9 +67,9 @@ async function add_item(item, collection)
  */
 async function get_items(collection) {
   try {
-      const values = await collection.find({}).toArray();
+      const VALUES = await collection.find({}).toArray();
       console.log('Alle Werte der Collection abgerufen');
-      return values;
+      return VALUES;
     } catch (err) {
       console.error('Fehler beim Abrufen der Werte aus der Collection:', err);
       return null;
@@ -83,9 +83,9 @@ async function get_items(collection) {
  */
 async function delete_item(id, collection) {
   try {
-    const result = await collection.deleteOne({ _id: new ObjectId(id) }); // Datensatz anhand der ID löschen
+    const RESULT = await collection.deleteOne({ _id: new ObjectId(id) }); // Datensatz anhand der ID löschen
 
-    if (result.deletedCount === 1) {
+    if (RESULT.deletedCount === 1) {
       return { message: 'Datensatz erfolgreich gelöscht' };
     } else {
       return { message: 'Datensatz nicht gefunden' };
@@ -103,12 +103,12 @@ async function delete_item(id, collection) {
  */
 async function update_item(id, newData, collection) {
   try {
-    const result = await collection.updateOne(
+    const RESULT = await collection.updateOne(
       { _id: new ObjectId(id) }, 
       { $set: newData } 
     );
 
-    if (result.modifiedCount === 1) {
+    if (RESULT.modifiedCount === 1) {
       return { message: 'Datensatz erfolgreich aktualisiert' };
     } else {
       return { message: 'Datensatz nicht gefunden' };
@@ -131,10 +131,10 @@ ROUTER.use((_req, res, next) => {
 ROUTER.get('/stations', async function(_req, res)
 {
   try {
-    const stations = await get_items(station_collection); 
+    const STATIONS = await get_items(station_collection); 
 
-    if (stations) {
-      res.json(stations); 
+    if (STATIONS) {
+      res.json(STATIONS); 
     } else {
       res.status(404).json({ message: 'Keine Stationen gefunden' });
     }
@@ -180,11 +180,11 @@ ROUTER.post('/delete_station', function(req, res) {
 
 ROUTER.post('/update_station', function(req, res) {
   const ID = req.body.id;
-  let newData = {
+  let new_data = {
     geojson: req.body.geojson
   };
     
-    update_item(ID, newData, station_collection);
+    update_item(ID, new_data, station_collection);
     res.send()
 });
 
@@ -194,10 +194,10 @@ ROUTER.post('/update_station', function(req, res) {
 ROUTER.get('/tours', async function(_req, res)
 {
   try {
-    const tours = await get_items(tour_collection); 
+    const TOURS = await get_items(tour_collection); 
 
-    if (tours) {
-      res.json(tours); 
+    if (TOURS) {
+      res.json(TOURS); 
     } else {
       res.status(404).json({ message: 'Keine Touren gefunden' });
     }
@@ -223,7 +223,7 @@ ROUTER.post('/delete_tour', function(req, res) {
 
 ROUTER.post('/update_tour', function(req, res) {
   const ID = req.body.id;
-  let newData = {
+  let new_data = {
     name: req.body.name,
     stations: req.body.stations,
     segments: req.body.segments,
@@ -231,7 +231,7 @@ ROUTER.post('/update_tour', function(req, res) {
     distance: req.body.distance
   };
     
-    update_item(ID, newData, tour_collection);
+    update_item(ID, new_data, tour_collection);
     res.send()
 });
 
@@ -250,8 +250,8 @@ async function getRouting(waypoints){
   const API_URL = `https://graphhopper.com/api/1/route?point=${waypoints.map(wp => `${wp.lat},${wp.lng}`).join('&point=')}&vehicle=bike&optimize="true"&points_encoded=false&key=${API_KEY}`;
 
   try {
-      const response = await fetch(API_URL);
-      const DATA = await response.json();
+      const RESPONSE = await fetch(API_URL);
+      const DATA = await RESPONSE.json();
       return DATA;
   } catch (error) {
       console.error('Fehler beim GRAPHHOPPER_API-Aufruf:', error);
@@ -260,12 +260,12 @@ async function getRouting(waypoints){
 }
 
 ROUTER.post('/routing', async function(req, res) {
-  const waypoints = req.body.waypoints;
+  const WAYPOINTS = req.body.waypoints;
   try {
-    const route = await getRouting(waypoints); 
+    const ROUTE = await getRouting(WAYPOINTS); 
 
-    if (route) { 
-      res.json(route); 
+    if (ROUTE) { 
+      res.json(ROUTE); 
     } else {
       res.status(404).json({ message: 'Keine Route gefunden' });
     }
