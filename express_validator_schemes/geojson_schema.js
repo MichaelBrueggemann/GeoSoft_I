0caa0f5f9c375185acc819d1d02abe0b7e0d711a
michael_b <m.brueggemann7@web.de>
2023-08-18 09:49:35 +0200

    mehr validierungscases ergänzt

--- express_validator_schemes/geojson_schema.js
+++ express_validator_schemes/geojson_schema.js
@@ -1,6 +1,6 @@
 "use strict"
 
-/* This schema defines a valid GeoJSOn input. 
+/* This schema defines a valid GeoJSON input. 
 This schema is documented more then the other schema to help understanding the used library*/
 const OUR_SPECIAL_GEOJSON_SCHEMA = 
 {
@@ -8,24 +8,71 @@ const OUR_SPECIAL_GEOJSON_SCHEMA =
         // the validator method "equals(str, comparison)" from validator.js
         equals: {
 
-        // message forwarded to the user
+        // message forwarded to the user. A so called "field-level message"
         errorMessage: "GeoJSON muss Typ 'Feature' haben!",
             // arguments passed to "equals()" as the "comparison" argument
             options: "Feature"
         } 
     },
+    'properties': {
+        notEmpty: {
+            errorMessage: "Die GeoJSON muss ein Feld 'properties' besitzen. Hier müssen die Schlüssel 'name' und 'description' existieren!",
+
+            // "bail" aborts the further validation proccess, if this validator isn't satisfied but thus also not sending any other error message
+            bail: true
+        }
+    },
     'properties.name': {
         trim: true,
         notEmpty: {
             errorMessage: "Der Name darf nicht leer sein!",
-            // "bail" aborts the further validation proccess, if this validator isn't satisfied
-            bail: true
         },
         isAlphanumeric: {
             errorMessage: "Der Name darf nur Buchstaben oder Zahlen enthalten!",
-            ignore: " -_"
-            
+            // this array contains the arguments that are forwarded to "isAlphanumeric(str, [locale, options])" from "validator.js"
+            options: ["de-DE", {ignore: " -_"}]
         },
+    },
+    'properties.description': {
+        trim: true,
+        notEmpty: {
+            errorMessage: "Die Beschreibung darf nicht leer sein!",
+        },
+        isAlphanumeric: {
+            errorMessage: "Die Beschreibung darf nur Buchstaben oder Zahlen enthalten!",
+            // this array contains the arguments that are forwarded to "isAlphanumeric(str, [locale, options])" from "validator.js"
+            options: ["de-DE", {ignore: " -_"}]
+        },
+    },
+    'properties.url': {
+        optional: true,
+    },
+    'geometry':{
+        notEmpty: {
+            errorMessage: "Die GeoJSON muss ein Feld 'geometry' besitzen. Hier müssen die Schlüssel 'coordinates' und 'type' existieren!",
+
+            // "bail" aborts the further validation proccess, if this validator isn't satisfied but thus also not sending any other error message
+            bail: true
+        }
+    },
+    'geometry.coordinates.*': {
+        trim: true,
+        notEmpty: {
+            errorMessage: "Die Koordinaten dürfen nicht leer sein!",
+        },
+        // TODO: Testen warum Floats nicht richtig erkannt werden
+        custom: {
+            errorMessage: "Die Koordinaten müssen Längen- und Breitengrad enthalten!",
+            options: {
+                // custom validator function
+                function(value)
+                {
+                    console.log("value", value)
+                    return /^\d+\.\d+/.test(value)
+                }
+            }
+        },
+        
     },
     'geometry.type': {
         /* this is a custom validator method, that isn't built-in in validator.js.
