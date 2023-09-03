a1394e09e9716e2acd76ede2114a0e8c82a6d9b5
tciroth <timciroth@uni-muenster.de>
2023-09-03 15:07:14 +0200

    Tourvalidierung von name und stations

--- validation_schemes/tour_schemas.js
+++ validation_schemes/tour_schemas.js
@@ -4,7 +4,17 @@ const JOI = require("joi")
 const {GEOJSON_SCHEMA} = require ("./joi_schemas")
 
 
-const TOUR_SCHEMA = JOI.object()
+const TOUR_SCHEMA = JOI.object(
+    {
+        // the RegEx allows letters, numbers and whitespace
+        name: JOI.string().pattern(/^[a-zA-Z0-9\s]*$/).required(),
+        stations: JOI.array().items(
+            GEOJSON_SCHEMA.keys({
+                _id: JOI.string().pattern(/^[a-zA-Z0-9\s]*$/).required(),
+            })
+            ).min(2).required(),
+    }
+).required().unknown(true)
 
 
 module.exports = {TOUR_SCHEMA}
